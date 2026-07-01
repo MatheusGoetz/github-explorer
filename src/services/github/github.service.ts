5fcf01c18bb34b05bf492bca606f3ba706d1abaa
Matheus Goetz <matheusgoetz@gmail.com>
2026-06-30 21:15:15 -0300

    refactor: separando services e lib

--- src/services/github/github.service.ts
+++ src/services/github/github.service.ts
@@ -1,8 +1,8 @@
-import { api } from "./api";
+import { api } from "@/lib/axios"
 import type { GithubUser } from "@/types";
 
 export async function getUser(username: string): Promise<GithubUser> {
   const { data } = await api.get(`/users/${username}`);
 
-  return data;
+  return data
 }
